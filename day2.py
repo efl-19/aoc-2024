8231dd9940688d518d8fe68a14284b56fe433eda
Eric Lloyd <elloyd@spotify.com>
2024-12-02 11:46:09 +0100

    day2 less lines better readability

--- day2.py
+++ day2.py
@@ -15,12 +15,12 @@ def is_safe_enough(level: list[int]) -> bool:
     return is_safe(level) or any(is_safe(level[:i] + level[i + 1:]) for i in range(len(level)))
 
 
+def tuple_sum(t) -> tuple[int, int]:
+    return reduce(lambda count, safe: (count[0] + safe[0], count[1] + safe[1]), t, (0, 0))
+
+
 if __name__ == '__main__':
-    with open('input/day2.txt', 'r') as input_data:
-        levels = (
-            [int(l) for l in report.strip().split(" ")]
-            for report in input_data.readlines()
-        )
-    
-    mapped = map(lambda level: (is_safe(level), is_safe_enough(level)), levels)
-    print(reduce(lambda count, safe: (count[0] + safe[0], count[1] + safe[1]), mapped, (0, 0)))
+    with open('input/day2.txt', 'r') as xs:
+        levels = ([int(l) for l in r.strip().split(" ")] for r in xs.readlines())
+
+    print(tuple_sum(map(lambda level: (is_safe(level), is_safe_enough(level)), levels)))
