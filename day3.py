b5edf8d0dd6b5cb2df7d71a2954af985b38761d2
Eric Lloyd <elloyd@spotify.com>
2024-12-03 08:58:15 +0100

    day 3 simplify

--- day3.py
+++ day3.py
@@ -1,28 +1,20 @@
 import re
 
 if __name__ == '__main__':
-    with open('input/day3.txt', 'r') as xs:
-        lines = xs.readlines()
-
     s1, s2 = 0, 0
-    p1, p2 = r'mul\((\d+),(\d+)\)', r'mul\(\d+,\d+\)|do\(\)|don\'t\(\)'
     enabled = True
-    for line in lines:
-        # part 1
-        ms_1 = re.findall(p1, line)
-        for a, b in ms_1:
-            s1 += int(a) * int(b)
-
-        # part 2
-        ms_2 = re.findall(p2, line)
-        for m in ms_2:
-            if m == 'do()':
-                enabled = True
-            elif m == 'don\'t()':
-                enabled = False
-            else:  # mul(a, b)
-                if enabled:
-                    a, b = re.match(p1, m).groups()
-                    s2 += int(a) * int(b)
+    with open('input/day3.txt', 'r') as xs:
+        for line in xs.readlines():
+            ms = re.findall('mul\(\d+,\d+\)|do\(\)|don\'t\(\)', line)
+            for m in ms:
+                if m == 'do()':
+                    enabled = True
+                elif m == 'don\'t()':
+                    enabled = False
+                else:  # mul(a, b)
+                    a, b = map(int, re.match('mul\((\d+),(\d+)\)', m).groups())
+                    s1 += a * b
+                    if enabled:
+                        s2 += a * b
 
     print(s1, s2)
