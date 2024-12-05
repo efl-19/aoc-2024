9f185aad9dd907dc7972f01d083559f1b1c0e1a7
Eric Lloyd <elloyd@spotify.com>
2024-12-05 10:35:09 +0100

    day 5 better var names

--- day5.py
+++ day5.py
@@ -17,12 +17,12 @@ if __name__ == '__main__':
     for a, b in ordering_rules:
         ORDER_MAP.setdefault(a, []).append(b)
 
-    sum_middles_sorted, sum_middle_after_sort = 0, 0
+    sum_middles_sorted, sum_middles_after_sort = 0, 0
     for update in updates:
         sorted_update = sorted(update, key=cmp_to_key(custom_cmp))
         if update == sorted_update:  # part 1
             sum_middles_sorted += update[int((len(update) - 1) / 2)]
         else:  # part 2
-            sum_middle_after_sort += sorted_update[int((len(sorted_update) - 1) / 2)]
+            sum_middles_after_sort += sorted_update[int((len(sorted_update) - 1) / 2)]
 
-    print(sum_middles_sorted, sum_middle_after_sort)
+    print(sum_middles_sorted, sum_middles_after_sort)
