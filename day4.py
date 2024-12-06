8f5ed04cff34780429159e6504099c88400a5ad2
Eric Lloyd <elloyd@spotify.com>
2024-12-06 09:43:48 +0100

    fix i,j in day4

--- day4.py
+++ day4.py
@@ -5,20 +5,20 @@ def is_word_cross(x1: str, x2: str, word: str) -> bool:
 class Grid:
     def __init__(self, grid: list[list[str]]):
         self.grid = grid
-        self.column_count = len(self.grid[0])
-        self.row_count = len(self.grid)
+        self.row_count = len(self.grid[0])
+        self.column_count = len(self.grid)
 
     def _inbound(self, x: int, y: int):
         return 0 <= x < self.row_count and 0 <= y < self.column_count
 
     def _neighbor(self, x: int, y: int, dx: int, dy: int) -> str | None:
         nx, ny = x + dx, y + dy
-        return self.grid[nx][ny] if self._inbound(nx, ny) else None
+        return self.grid[ny][nx] if self._inbound(nx, ny) else None
 
     def _search_word(self, x: int, y: int, word: str, dx: int, dy: int) -> bool:
         for i, char in enumerate(word):
             nx, ny = x + dx * i, y + dy * i
-            if not self._inbound(nx, ny) or self.grid[nx][ny] != char:
+            if not self._inbound(nx, ny) or self.grid[ny][nx] != char:
                 return False
         return True
 
@@ -28,7 +28,7 @@ class Grid:
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (1, -1), (-1, 1)]
         for i in range(self.row_count):
             for j in range(self.column_count):
-                if self.grid[i][j] == word[0]:
+                if self.grid[j][i] == word[0]:
                     for dx, dy in directions:
                         if self._search_word(i, j, word, dx, dy):
                             count += 1
@@ -39,7 +39,7 @@ class Grid:
         count = 0
         for i in range(self.row_count):
             for j in range(self.column_count):
-                if self.grid[i][j] == word[1]:
+                if self.grid[j][i] == word[1]:
                     top_left = self._neighbor(i, j, -1, -1)
                     bottom_right = self._neighbor(i, j, 1, 1)
                     top_right = self._neighbor(i, j, -1, 1)
