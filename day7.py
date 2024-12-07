bb2465d642d2af25454538f161b3b9eaa60f9ac7
Eric Lloyd <elloyd@spotify.com>
2024-12-07 10:16:13 +0100

    day7 small refactor

--- day7.py
+++ day7.py
@@ -1,28 +1,28 @@
 from itertools import product
 from operator import add, mul
-from typing import Callable
+from typing import Callable, Iterable
 
 
-def solve(equations: list[tuple[int, tuple[int]]], operators: tuple[Callable]) -> int:
+def solve(equations: tuple[tuple[int, tuple[int, ...]], ...], operators: Iterable[Callable]) -> int:
     matches: set[int] = set()
-    for t, ns in equations:
-        for p in product(operators, repeat=len(ns) - 1):
-            result = ns[0]
-            for i, n in enumerate(ns[1:]):
-                result = p[i](result, n)
-            if result == t:  # found a combination that equals to t
-                matches.add(t)
+    for test_result, numbers in equations:
+        for fn in product(operators, repeat=len(numbers) - 1):
+            result = numbers[0]
+            for i, n in enumerate(numbers[1:]):
+                result = fn[i](result, n)
+            if result == test_result:  # found a combination that equals to t
+                matches.add(test_result)
                 break
 
     return sum(matches)
 
 
 if __name__ == '__main__':
-    equations = []
     with open('input/day7.txt', 'r') as xs:
-        for l in xs.readlines():
-            t, ns = l.split(":")
-            equations.append((int(t), tuple(map(int, ns.strip().split(" ")))))
+        eqs = tuple(
+            (int(t), tuple(map(int, ns.strip().split(" "))))
+            for t, ns in (l.split(":") for l in xs.readlines())
+        )
 
-    print(solve(equations, (add, mul)))  # part 1
-    print(solve(equations, (add, mul, lambda a, b: int(str(a) + str(b)))))  # part 2
+    print(solve(eqs, (add, mul)))  # part 1
+    print(solve(eqs, (add, mul, lambda a, b: int(str(a) + str(b)))))  # part 2
