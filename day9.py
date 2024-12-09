77ab49a52a2911eff2e45b18bc7aeb9bad6472a5
Eric Lloyd <elloyd@spotify.com>
2024-12-09 17:23:03 +0100

    day9 part 2

--- day9.py
+++ day9.py
@@ -1,6 +1,14 @@
+from itertools import chain
+
+
 def find_all_indices(blocks: list[str], predicate: callable) -> list[int]:
     return [i for i, char in enumerate(blocks) if predicate(char)]
 
+def find_last_index(l: list[list[str]], element: list[str]) -> int:
+    for index in range(len(l) - 1, -1, -1):
+        if l[index] == element:
+            return index
+    raise ValueError("Element not found in list")
 
 def checksum_p1(blocks: list[str]) -> int:
     final_blocks = blocks.copy()
@@ -19,10 +27,37 @@ def checksum_p1(blocks: list[str]) -> int:
     return c_sum
 
 
+def checksum_p2(blocks: list[list[str]]) -> int:
+    final_blocks = blocks.copy()
+
+    for block in reversed(blocks):
+        if block[0].isdigit():
+            for fi, f_block in enumerate(final_blocks):
+                if f_block[0] == '.' and len(f_block) >= len(block):  # possible swap
+                    li = find_last_index(final_blocks, block)
+                    if fi >= li:  # no more swaps needed
+                        break
+                    if len(f_block) == len(block):  # simple swap
+                        final_blocks[fi], final_blocks[li] = final_blocks[li], final_blocks[fi]
+                    else:  # complex swap
+                        final_blocks[fi], final_blocks[li] = final_blocks[li], len(block) * ['.']
+                        remaining_dots = (len(f_block) - len(block)) * ['.']
+                        final_blocks.insert(fi + 1, remaining_dots)
+                    break
+
+    c_sum = 0
+    for i, id in enumerate(chain.from_iterable(final_blocks)):
+        if not id.isdigit():
+            continue
+        c_sum += int(id) * i
+    return c_sum
+
+
 if __name__ == '__main__':
     with open('input/day9.txt', 'r') as xs:
         digits = list(map(int, xs.read().strip()))
 
+    # part 1
     id = 0
     blocks = []
     for i, digit in enumerate(digits):
@@ -35,3 +70,15 @@ if __name__ == '__main__':
                 blocks.append(".")
 
     print(checksum_p1(blocks))
+
+    # part 2
+    id = 0
+    blocks = []
+    for i, digit in enumerate(digits):
+        if i % 2 == 0:  # length of a file
+            blocks.append([str(id)] * digit)
+            id += 1
+        elif digit != 0:  # free space
+            blocks.append(["."] * digit)
+
+    print(checksum_p2(blocks))
