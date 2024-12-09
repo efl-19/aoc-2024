cc4db8cae62fd4f502b7cacf716330010c1e7d0a
Eric Lloyd <elloyd@spotify.com>
2024-12-09 09:16:55 +0100

    day9 part1

--- day9.py
+++ day9.py
@@ -2,14 +2,13 @@ def find_all_indices(blocks: list[str], predicate: callable) -> list[int]:
     return [i for i, char in enumerate(blocks) if predicate(char)]
 
 
-def checksum(blocks: str) -> int:
-    final_blocks = list(blocks)
+def checksum_p1(blocks: list[str]) -> int:
+    final_blocks = blocks.copy()
     free_space_indices = find_all_indices(final_blocks, lambda x: x == '.')
     digits_indices = find_all_indices(final_blocks, lambda x: x.isdigit())
     for fs_i, d_i in zip(free_space_indices, reversed(digits_indices)):
         if fs_i >= d_i:  # no more swaps needed
             break
-        print(fs_i, d_i)
         final_blocks[fs_i], final_blocks[d_i] = final_blocks[d_i], final_blocks[fs_i]  # swap
 
     c_sum = 0
@@ -24,15 +23,15 @@ if __name__ == '__main__':
     with open('input/day9.txt', 'r') as xs:
         digits = list(map(int, xs.read().strip()))
 
-    print(digits)
-
     id = 0
-    blocks = ""
+    blocks = []
     for i, digit in enumerate(digits):
         if i % 2 == 0:  # length of a file
-            blocks += str(id) * digit
+            for _ in range(digit):
+                blocks.append(str(id))
             id += 1
         else:  # free space
-            blocks += "." * digit
-    print(blocks)
-    print(checksum(blocks))
+            for _ in range(digit):
+                blocks.append(".")
+
+    print(checksum_p1(blocks))
