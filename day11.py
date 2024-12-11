d1b3c9e98a2917ae6829339ad2bed2f84ee9dd32
Eric Lloyd <elloyd@spotify.com>
2024-12-11 14:10:50 +0100

    day 11 cleanups

--- day11.py
+++ day11.py
@@ -5,21 +5,21 @@ if __name__ == '__main__':
         for stone in map(int, xs.read().split()):
             stones_count[stone] = stones_count.get(stone, 0) + 1
 
-    for i in range(1, 75 + 1):
+    for blink in range(1, 75 + 1):
         previous_stones_count = stones_count.copy()
         for stone, curr_count in previous_stones_count.items():
             stones_count[stone] = stones_count.get(stone, 0) - curr_count  # remove the stones we will transform
-            str_stone = str(stone)
+            ss, len_ss = str(stone), len(str(stone))
             if stone == 0:
                 stones_count[1] = stones_count.get(1, 0) + curr_count
-            elif len(str_stone) % 2 == 0:
-                left, right = str_stone[:len(str_stone) // 2], str_stone[len(str_stone) // 2:]
-                stones_count[int(left)] = stones_count.get(int(left), 0) + curr_count
-                stones_count[int(right)] = stones_count.get(int(right), 0) + curr_count
+            elif len_ss % 2 == 0:
+                left, right = int(ss[:len_ss // 2]), int(ss[len_ss // 2:])
+                stones_count[left] = stones_count.get(left, 0) + curr_count
+                stones_count[right] = stones_count.get(right, 0) + curr_count
             else:
                 stones_count[stone * 2024] = stones_count.get(stone * 2024, 0) + curr_count
 
-        if i == 25:  # part 1
+        if blink == 25:  # part 1
             print(sum(stones_count.values()))
 
     print(sum(stones_count.values()))  # part 2
