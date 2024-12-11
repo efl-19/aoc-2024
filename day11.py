d9f0055e269dad9222363d80994ae1cfb5868a0a
Eric Lloyd <elloyd@spotify.com>
2024-12-11 12:25:39 +0100

    day 11 part 1 and 2 optimized

--- day11.py
+++ day11.py
@@ -17,10 +17,34 @@ def blink(stones: list[int]) -> list[int]:
 if __name__ == '__main__':
     with open('input/day11.txt', 'r') as xs:
         stones = list(map(int, xs.read().split()))
-    blink_count = 25
-    for i in range(blink_count):
-        stones = blink(stones)
 
-    print(len(stones))
+    stones_count: dict[int, int] = {}
+    for stone in stones:
+        stones_count[stone] = stones_count.get(stone, 0) + 1
+
+    for i in range(1, 75 + 1):
+        previous_stones_count = stones_count.copy()
+        for stone, curr_count in previous_stones_count.items():
+            stones_count[stone] = stones_count.get(stone, 0) - curr_count
+            str_stone = str(stone)
+            if stone == 0:
+                stones_count[1] = stones_count.get(1, 0) + curr_count
+            elif len(str_stone) % 2 == 0:
+                left, right = str_stone[:len(str_stone) // 2], str_stone[len(str_stone) // 2:]
+                stones_count[int(left)] = stones_count.get(int(left), 0) + curr_count
+                stones_count[int(right)] = stones_count.get(int(right), 0) + curr_count
+            else:
+                stones_count[stone * 2024] = stones_count.get(stone * 2024, 0) + curr_count
+
+        if i == 25:  # part 1
+            print(sum(stones_count.values()))
+
+    print(sum(stones_count.values()))  # part 2
+
+
+
+
+
+
 
 
